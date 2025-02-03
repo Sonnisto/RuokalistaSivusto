02df62bf552162fb61bc39a2ac838c78fac4af9b
Joona Saarentola <joosaa@utu.fi>
2025-02-03 13:59:27 +0200

    Korjattu uuden JSON-formaatin takia tullut ongelma

--- BackEnd.py
+++ BackEnd.py
@@ -6,15 +6,17 @@ from Lemminkaisenkatu import fetch_sodexo_data
 
 app = Flask(__name__)
 
+#1.2.2025? URL CHANGE FROM /MODULES/JSON/JSON TO MENUAPI/FEED/JSON
+
 # JSON URLs for each restaurant
 JSON_URLS = [
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1920&language=fi",    #Assarin-ullakko
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1970&language=fi",    #Macciavelli
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1995&language=fi",    #Galilei
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1980&language=fi",    #Null
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1985&language=fi",    #Delica
-    "https://www.unica.fi/modules/json/json/Index?costNumber=2000&language=fi",    #Null
-    "https://www.unica.fi/modules/json/json/Index?costNumber=1900&language=fi",    #Kisälli
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1920&language=fi",          #Assarin ullakko
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1970&language=fi",          #Macciavelli
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1995&language=fi",          #Galilei
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1980&language=fi",          #Dental
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1985&language=fii",         #Delica
+    "https://www.unica.fi/menuapi/feed/json?costNumber=2000&language=fi",          #Linus
+    "https://www.unica.fi/menuapi/feed/json?costNumber=1900&language=fi",          #Kisälli
     
 ]
 
@@ -24,12 +26,11 @@ def fetch_menu_data(json_url):
         response = requests.get(json_url)
         response.raise_for_status()  # Raise an HTTPError for bad responses
         data = response.json()
-
-
         if data is None or not data["MenusForDays"]:
             return None
     
         return data
+    
     except requests.RequestException as e:
         print(f"Error {json_url}, cause {e}")
         return None
@@ -56,15 +57,11 @@ def get_current_day_in_finnish():
     return days_of_week[current_day_index] 
 
 
-
 # Home route: Redirects to the current day's menu
 @app.route('/')
 def home():
     current_day_finnish = get_current_day_in_finnish()
     return redirect(url_for('show_day', day_name=current_day_finnish))
-
-
-
 
 # Route to show the menu for a specific day
 @app.route('/day/<day_name>')
@@ -98,40 +95,43 @@ def show_day(day_name):
         restaurant_name = restaurant_data.get('RestaurantName', 'Unknown Restaurant')
         day_menus = get_menus_for_day(restaurant_data, requested_date)
 
-
+        
         #Gets the opening_hours and formats it {Opening_time}-{Closing_time}
         lunch_time_raw = restaurant_data.get('MenusForDays')[0].get('LunchTime', None)
 
         if lunch_time_raw is None:
             continue
-        
 
+        lunch_time_hyphen_normalized = lunch_time_raw.replace("–", "-")
 
-        time_parts = lunch_time_raw.split('-')
+        if lunch_time_hyphen_normalized is None:
+            continue
+    
+        time_parts = lunch_time_hyphen_normalized.strip().split('-')
+    
         start_time = time_parts[0].strip()
-        end_time = time_parts[1].split()[0].strip()
+        end_time = time_parts[1]
         clean_lunch_time = f"{start_time}-{end_time}"
 
 
-        all_restaurant_content.append({
-            'restaurant_name': restaurant_name,
-            'day_menus': day_menus,
-            'Opening_hours': clean_lunch_time,
-        })
-
-
-
+        #Append the restaurant to the list if there is a menu. Otherwise don't show it on the HTML
+        if day_menus: 
+            all_restaurant_content.append({
+                'restaurant_name': restaurant_name,
+                'day_menus': day_menus,
+                'Opening_hours': clean_lunch_time,
+         })
 
     sodexo_data = fetch_sodexo_data()  # Call the function to fetch Sodexo data
     if sodexo_data:
         sodexo_menus = get_menus_for_day(sodexo_data, requested_date)  # Extract the menus for the requested date
-        all_restaurant_content.append({
-            'restaurant_name': sodexo_data.get('RestaurantName'),
-            'day_menus': sodexo_menus,
-            'Opening_hours': "10:30-13:00"   #HARD-CODED Opening-hours, because json-format doesnt include it
-        })
 
-
+        if sodexo_menus:
+            all_restaurant_content.append({
+                'restaurant_name': sodexo_data.get('RestaurantName'),
+                'day_menus': sodexo_menus,
+                'Opening_hours': "10:30-13:00"   #HARD-CODED Opening-hours, because json-format doesnt include it
+            })
 
 
     # Set up navigation for previous and next day
